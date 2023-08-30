641d3a786d6431324a7ce0c0ce24963513ab5008
Ar.kuzmin <ar.kuzmin@yandex.ru>
2023-08-30 23:12:21 +0700

    Initial commit

---
+++ src/main/java/com/example/BillingCentreTest/BillingCentreTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.BillingCentreTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingCentreTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingCentreTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BillingCentreTest/BillingCentreTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BillingCentreTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingCentreTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
